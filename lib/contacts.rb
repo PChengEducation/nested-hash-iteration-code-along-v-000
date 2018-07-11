97bc90ca0e66a41cac6b7a3fc070c1e2f9932f67
Pan Cheng <pchengeducation@hotmail.com>
2018-07-11 00:28:02 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,6 +20,7 @@ contacts.each do |person, contact_details_hash|
     if attribute == :favorite_ice_cream_flavors
       data.delete_if {|ice_cream| ice_cream == "strawberry"}
     end
+  end
     #binding.pry
   end
 end
